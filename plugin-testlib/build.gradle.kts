ad2e8f5cda83cc3b4e3def006768ec27912fad2a
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-09-13 17:31:03 +0300

    Remove custom duplicate strategies

--- plugin-testlib/build.gradle.kts
+++ plugin-testlib/build.gradle.kts
@@ -34,9 +34,3 @@ dependencies {
     implementation(gradleApi())
     implementation(gradleTestKit())
 }
-
-//TODO:2021-07-22:alexander.yevsyukov: Turn to WARN and investigate duplicates.
-// see https://github.com/SpineEventEngine/base/issues/657
-val dupStrategy = DuplicatesStrategy.INCLUDE
-tasks.processTestResources.get().duplicatesStrategy = dupStrategy
-//tasks["sourceJar"].get().duplicatesStrategy = dupStrategy
