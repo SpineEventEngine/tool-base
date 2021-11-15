6191242273783d1f3bff6950fbb3e15be3fbd60e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-11-15 17:59:02 +0200

    Add `protobufConvention` and `generatedDir` properties

--- plugin-base/src/main/kotlin/io/spine/tools/gradle/project/ProjectExtensions.kt
+++ plugin-base/src/main/kotlin/io/spine/tools/gradle/project/ProjectExtensions.kt
@@ -28,12 +28,16 @@
 
 package io.spine.tools.gradle.project
 
+import com.google.protobuf.gradle.ProtobufConvention
 import io.spine.tools.fs.DefaultPaths
 import io.spine.tools.fs.DescriptorsDir
+import io.spine.tools.fs.DirectoryName.generated
 import io.spine.tools.gradle.Artifact
 import io.spine.tools.gradle.SourceSetName
+import io.spine.tools.gradle.resolve
 import java.io.File
 import java.nio.file.Path
+import java.nio.file.Paths
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.SourceSet
@@ -118,3 +122,33 @@ public fun Project.descriptorSetFile(ss: SourceSetName): File {
  * Obtains the path to this file resolved under the passed directory.
  */
 private fun File.under(dir: Path): Path = dir.resolve(toString())
+
+@Suppress("DEPRECATION") /* We have to use `getConvention()` until
+        Protobuf Gradle Plugin migrates to new API. */
+public val Project.protobufConvention: ProtobufConvention
+    get() = convention.getPlugin(ProtobufConvention::class.java)
+
+/**
+ * Obtains the path to the directory which will be used for placing files generated
+ * from proto definitions.
+ */
+public val Project.generatedDir: Path
+    get() {
+        val fromConvention = protobufConvention.protobuf.generatedFilesBaseDir
+        if (fromConvention.startsWith(buildDir.toString())) {
+            /*
+               Ignore the default value specified by the plugin code because it "buries" the
+               generated code under the `build` directory.
+
+               We want the generated code more visible, and place it at the root of the project,
+               so it is seen as a "sibling" with the `src` directory.
+
+               This is the convention of our framework.
+            */
+            return projectDir.resolve(generated).toPath()
+        }
+
+        /* If custom value was set by the programmer in the `protobuf` closure of the build
+           script, use the specified path instead of the framework convention. */
+        return Paths.get(fromConvention)
+    }
