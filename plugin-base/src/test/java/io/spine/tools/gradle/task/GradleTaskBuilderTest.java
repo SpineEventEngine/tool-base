a7cd6c6ceac78c5089b7c48dc5e582a41da2e0d4
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-11-16 22:13:33 +0200

    Move test from `mc-java`

---
+++ plugin-base/src/test/java/io/spine/tools/gradle/task/GradleTaskBuilderTest.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2021, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.gradle.task;
+
+import io.spine.tools.gradle.testing.NoOp;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import static com.google.common.truth.Truth.assertThat;
+import static io.spine.testing.Assertions.assertIllegalState;
+import static io.spine.tools.gradle.task.BaseTaskName.clean;
+import static io.spine.tools.gradle.task.GivenTaskName.annotateProto;
+import static io.spine.tools.gradle.task.GivenTaskName.preClean;
+import static io.spine.tools.gradle.task.GivenTaskName.verifyModel;
+import static io.spine.tools.gradle.task.GradleTask.Builder;
+import static io.spine.tools.gradle.task.JavaTaskName.classes;
+import static io.spine.tools.gradle.task.JavaTaskName.compileJava;
+import static io.spine.tools.gradle.task.ProtobufTaskName.generateProto;
+import static io.spine.tools.gradle.task.ProtobufTaskName.generateTestProto;
+import static io.spine.tools.gradle.testing.GradleProject.javaPlugin;
+import static io.spine.tools.gradle.testing.NoOp.action;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * Tests {@link GradleTask.Builder}.
+ */
+@DisplayName("`GradleTask.Builder` should")
+class GradleTaskBuilderTest {
+
+    private Project project;
+
+    @BeforeEach
+    void setUp() {
+        project = ProjectBuilder.builder()
+                                .build();
+        project.getPluginManager()
+               .apply(javaPlugin());
+    }
+
+    @Test
+    @DisplayName("create task dependant on all tasks of given name")
+    void createTaskDependantOnAllTasksOfGivenName() {
+        Project subProject = ProjectBuilder.builder()
+                .withParent(project)
+                .build();
+        subProject.getPluginManager()
+                  .apply(javaPlugin());
+        GradleTask task = GradleTask.newBuilder(annotateProto, NoOp.action())
+                .insertAfterAllTasks(compileJava)
+                .applyNowTo(subProject);
+        TaskContainer subProjectTasks = subProject.getTasks();
+        Task newTask = subProjectTasks.findByName(task.getName().name());
+        assertNotNull(newTask);
+        Collection<?> dependencies = newTask.getDependsOn();
+
+        assertThat(dependencies)
+                .containsAtLeast(
+                        subProjectTasks.findByName(compileJava.name()),
+                        project.getTasks()
+                               .findByName(compileJava.name())
+                );
+    }
+
+    @Test
+    @DisplayName("create task and insert before other")
+    void createTaskAndInsertBeforeOther() {
+        GradleTask.newBuilder(verifyModel, NoOp.action())
+              .insertBeforeTask(classes)
+              .applyNowTo(project);
+        TaskContainer tasks = project.getTasks();
+        Task classes = tasks.findByName(JavaTaskName.classes.name());
+        assertNotNull(classes);
+        Task verifyModelTask = tasks.findByName(verifyModel.name());
+        assertThat(classes.getDependsOn())
+                .contains(verifyModelTask);
+    }
+
+    @Test
+    @DisplayName("create task and insert after other")
+    void createTaskAndInsertAfterOther() {
+        GradleTask.newBuilder(verifyModel, NoOp.action())
+              .insertAfterTask(compileJava)
+              .applyNowTo(project);
+        TaskContainer tasks = project.getTasks();
+        Task compileJavaTask = tasks.findByName(compileJava.name());
+        assertNotNull(compileJavaTask);
+        Task verifyModelTask = tasks.findByName(verifyModel.name());
+        assertNotNull(verifyModelTask);
+        assertThat(verifyModelTask.getDependsOn())
+                .contains(compileJavaTask.getName());
+    }
+
+    @Test
+    @DisplayName("ignore task dependency if no such task found")
+    void ignoreTaskDependencyIfNoSuchTaskFound() {
+        GradleTask.newBuilder(generateTestProto, NoOp.action())
+              .insertAfterAllTasks(generateProto)
+              .applyNowTo(project);
+        TaskContainer tasks = project.getTasks();
+        Task generateProtoTask = tasks.findByName(generateProto.name());
+        assertNull(generateProtoTask);
+        Task generateTestProtoTask = tasks.findByName(generateTestProto.name());
+        assertNotNull(generateTestProtoTask);
+    }
+
+    @Test
+    @DisplayName("not allow tasks without any connection to task graph")
+    void notAllowTasksWithoutAnyConnectionToTaskGraph() {
+        Builder builder = GradleTask.newBuilder(verifyModel, action());
+        assertIllegalState(() -> builder.applyNowTo(project));
+    }
+
+    @Test
+    @DisplayName("return build task description")
+    void returnBuildTaskDescription() {
+        GradleTask desc = GradleTask.newBuilder(preClean, NoOp.action())
+                .insertBeforeTask(clean)
+                .applyNowTo(project);
+        assertEquals(preClean, desc.getName());
+        assertEquals(project, desc.getProject());
+    }
+
+    @Test
+    @DisplayName("create task with given inputs")
+    void createTaskWithGivenInputs() throws IOException {
+        File input = new File(".").getAbsoluteFile();
+        FileCollection files = project.getLayout().files(input);
+        GradleTask.newBuilder(preClean, NoOp.action())
+              .insertBeforeTask(clean)
+              .withInputFiles(files)
+              .applyNowTo(project);
+        Task task = project.getTasks()
+                           .findByPath(preClean.name());
+        assertNotNull(task);
+        File singleInput = task.getInputs()
+                               .getFiles()
+                               .getFiles()
+                               .iterator()
+                               .next();
+        assertEquals(input.getCanonicalFile(), singleInput.getCanonicalFile());
+    }
+}
