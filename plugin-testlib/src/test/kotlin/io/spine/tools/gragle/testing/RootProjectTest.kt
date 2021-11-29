d06b75982ea45e8a03e0b49cc912529e122819c4
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-11-29 18:14:35 +0200

    Migrate test class to Kotlin

--- plugin-testlib/src/test/kotlin/io/spine/tools/gragle/testing/RootProjectTest.kt
+++ plugin-testlib/src/test/kotlin/io/spine/tools/gragle/testing/RootProjectTest.kt
@@ -23,21 +23,16 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package io.spine.tools.gradle.testing
 
-package io.spine.tools.gradle.testing;
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static com.google.common.truth.Truth.assertThat;
-
-@DisplayName("`ProjectRoot` utility should")
-class RootProjectTest {
+class `'RootProject' should` {
 
     @Test
-    @DisplayName("locate the project root")
-    void find() {
+    fun `locate the project root`() {
         assertThat(RootProject.dir().exists())
-                .isTrue();
+            .isTrue()
     }
 }
