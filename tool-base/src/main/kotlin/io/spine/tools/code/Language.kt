6c123ae6913ce7ca50b8cc72a418c89fd07deb1a
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-01-10 20:12:32 +0200

    Add `Language` from `ProtoData`
    ... and extend with support of multiple file extensions.

---
+++ tool-base/src/main/kotlin/io/spine/tools/code/Language.kt
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2021, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.code
+
+import io.spine.io.Glob
+import java.io.File
+import java.nio.file.Path
+
+/**
+ * A programming language or a specific syntax.
+ *
+ * For example, `"Java"`, `"Python 3.x"`, etc.
+ */
+public abstract class Language internal constructor(
+
+    /**
+     * Label to distinguish the language.
+     */
+    public val name: String,
+
+    /**
+     * Extensions of source code files on this language with or without the leading dot.
+     *
+     * Some languages may have more than one type of files, e.g. header files ".h" for C source
+     * code files (".c"). Also, conventions for some languages allow alternative extensions,
+     * e.g. ".cc" and ".cpp" for C++.
+     *
+     * The passed values are converted to lower case.
+     */
+    fileExtensions: List<String>,
+
+    /**
+     * Patterns one of which a language source file must match.
+     *
+     * If not specified, patterns matching [fileExtensions] will be created.
+     */
+    filePatterns: List<Glob>? = null
+) {
+
+    /**
+     * The extension of source code files in this language without the leading dot.
+     */
+    public val fileExtensions: List<String>
+
+    /**
+     * Pattern which source code files must match.
+     */
+    private val filePatterns: List<Glob>
+
+    init {
+        this.fileExtensions = fileExtensions
+            .map { it.replaceFirst(".", "") }
+            .map { it.lowercase() }
+            .toMutableList()
+            .sorted()
+        this.filePatterns = filePatterns ?: this.fileExtensions.map(Glob::extension)
+    }
+
+    /**
+     * Creates a syntactically valid one-line comment in this language.
+     *
+     * @param line
+     *          the contents of the comment
+     * @return a line which can be safely inserted into a code file.
+     */
+    public abstract fun comment(line: String): String
+
+    /**
+     * Checks if the given file is a code file in this language.
+     *
+     * If the passed instance is a directory, rather than a file, returns `false`.
+     */
+    public fun matches(file: File): Boolean = matches(file.toPath())
+
+    /**
+     * Checks if the given file is a code file in this language.
+     *
+     * If the passed instance is a directory, rather than a file, returns `false`.
+     */
+    public fun matches(file: Path): Boolean = filePatterns.any { it.matches(file) }
+
+    /**
+     * Returns the name of the language.
+     */
+    override fun toString(): String = name
+}
+
+/**
+ * A C-like language.
+ *
+ * Supports double-slash comments (`// <comment body>`).
+ */
+public class SlashCommentLanguage(
+    name: String,
+    fileExtensions: List<String>
+) : Language(name, fileExtensions) {
+
+    override fun comment(line: String): String = "// $line"
+}
+
+/**
+ * A collection of commonly used [Language]s.
+ *
+ * If this prepared set is not enough, users are encouraged to create custom [Language] types
+ * by either extending the class directly, or using one of its existing subtypes, such as
+ * [SlashCommentLanguage].
+ */
+public object CommonLanguages {
+
+    /**
+     * Any language will do.
+     *
+     * This instance indicates that any programming language can be accepted.
+     *
+     * Intended to be used for filtering source files by language via file name conventions.
+     * If no filtering required, but a [Language] is needed, use `CommonLanguages.any`.
+     *
+     * Does not support [comments][Language.comment].
+     */
+    @get:JvmName("any")
+    @JvmStatic
+    public val any: Language = object : Language("any language", listOf(""), listOf(Glob.any)) {
+        override fun comment(line: String): String {
+            throw UnsupportedOperationException("`$name` does not support comments.")
+        }
+    }
+
+    @get:JvmName("kotlin")
+    @JvmStatic
+    public val Kotlin: Language = SlashCommentLanguage("Kotlin", listOf("kt"))
+
+    @get:JvmName("java")
+    @JvmStatic
+    public val Java: Language = SlashCommentLanguage("Java", listOf("java"))
+
+    @get:JvmName("javaScript")
+    @JvmStatic
+    public val JavaScript: Language = SlashCommentLanguage("JavaScript", listOf("js"))
+}
---
+++ tool-base/src/test/kotlin/io/spine/tools/code/LanguageTest.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2022, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.code
+
+import com.google.common.truth.Truth.assertThat
+import io.spine.tools.code.CommonLanguages.Java
+import io.spine.tools.code.CommonLanguages.JavaScript
+import io.spine.tools.code.CommonLanguages.Kotlin
+import io.spine.tools.code.CommonLanguages.any
+import java.io.File
+import java.nio.file.Paths
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class `'Language' should` {
+
+    @Test
+    fun `return its name in the string form`() {
+        assertThat(Kotlin.toString()).isEqualTo("Kotlin")
+    }
+
+    @Test
+    fun `expose file pattern`() {
+        assertThat(Kotlin.fileExtensions).containsExactly("kt")
+        assertThat(Brainfuck().fileExtensions).containsExactly("b", "bf")
+    }
+
+    @Test
+    fun `format a test line as comments`() {
+        assertThat(Java.comment("Hey!")).isEqualTo("// Hey!")
+    }
+
+    @Test
+    fun `filter files by their extension`() {
+        // Passing the directory, rather than file.
+        assertThat(JavaScript.matches(File("Windows"))).isFalse()
+
+        // Passing file with extension.
+        assertThat(JavaScript.matches(File("safari.swift"))).isFalse()
+        
+        assertThat(JavaScript.matches(File("mozilla.js"))).isTrue()
+    }
+
+    @Test
+    fun `throw 'UnsupportedOperationException' for the synthetic 'any' language`() {
+        assertThrows<UnsupportedOperationException> { any.comment("kaboom") }
+    }
+
+    @Nested
+    inner class `support more than one file extension` {
+
+        private val cpp = SlashCommentLanguage("C++", listOf(".HPP", ".cc", ".CPP", "h"))
+
+        @Test
+        fun `sorting and lower-casing them on initialization`() {
+            assertThat(cpp.fileExtensions).containsExactly("cc", "cpp", "h", "hpp")
+        }
+
+        @Test
+        fun `matching all kinds of files`() {
+            assertMatches("1.cc")
+            assertMatches("2.h")
+            assertMatches("3.hpp")
+            assertMatches("4.cpp")
+        }
+
+        private fun assertMatches(file: String) {
+            val path = Paths.get(file)
+            assertThat(cpp.matches(path)).isTrue()
+        }
+    }
+}
+
+private class Brainfuck: Language("Brainfuck", listOf(".b", ".bf")) {
+    override fun comment(line: String): String = "[ $line ]"
+}
