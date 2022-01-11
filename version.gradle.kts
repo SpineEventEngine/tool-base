66aabdf0cdee8b4f8b71b78006a6f7f982d523cf
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-01-11 16:44:14 +0200

    Bump `base`

--- version.gradle.kts
+++ version.gradle.kts
@@ -24,5 +24,5 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val baseVersion: String by extra("2.0.0-SNAPSHOT.83")
+val baseVersion: String by extra("2.0.0-SNAPSHOT.84")
 val versionToPublish: String by extra("2.0.0-SNAPSHOT.89")
