d59fb6c31bd5de72d656033a259d8dc6e4ca8191
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-04-28 18:12:55 +0300

    Bump version -> `2.0.0-SNAPSHOT.93`

--- version.gradle.kts
+++ version.gradle.kts
@@ -25,4 +25,4 @@
  */
 
 val baseVersion: String by extra("2.0.0-SNAPSHOT.91")
-val versionToPublish: String by extra("2.0.0-SNAPSHOT.92")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.93")
