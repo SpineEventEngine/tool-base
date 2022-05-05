a810911b99993e26e53b10f75242b352fe519a2b
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-05-05 23:07:05 +0300

    Remove unused code
    Also:
     * Simplify code flow via applying extension functions.

--- tool-base/src/main/kotlin/io/spine/tools/code/manifest/KManifest.kt
+++ tool-base/src/main/kotlin/io/spine/tools/code/manifest/KManifest.kt
@@ -66,20 +66,17 @@ public open class KManifest(protected val impl: Manifest) {
          * Loads the manifest next to the given class.
          */
         public fun load(cls: Class<*>): KManifest {
-            val classFile = cls.simpleName + ".class"
-            val classResource = cls.getResource(classFile)!!
-
+            val classResource = cls.toResourceUrl()
             val urlConnection = classResource.openConnection()!!
             if (urlConnection is JarURLConnection) {
                 val manifest = urlConnection.manifest
                 return KManifest(manifest)
             }
-
-            val classResourcePath = classResource.toString()
-            if (!classResourcePath.startsWith("jar")) {
-                return loadNonJar(cls, classResourcePath)
+            if (!classResource.isInJar()) {
+                return loadNonJar(cls)
             }
-            return loadFromJar(classResourcePath)
+            throw IllegalStateException("Unable to load manifest file for" +
+                    " the class with the URL `$classResource`.")
         }
 
         /**
@@ -140,28 +137,45 @@ public open class KManifest(protected val impl: Manifest) {
     }
 }
 
-private fun loadFromJar(classPath: String): KManifest {
-    val manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) +
-            "/" + KManifest.RESOURCE_FILE
-    val url = URL(manifestPath)
-    val stream = url.openStream()
-    return KManifest.load(stream)
+/**
+ * Obtains URL for obtaining this class as a resource.
+ */
+private fun Class<*>.toResourceUrl(): URL {
+    val classFile = "$simpleName.class"
+    val classResource = getResource(classFile)!!
+    return classResource
 }
 
-private fun loadNonJar(cls: Class<*>, classResourcePath: String): KManifest {
-    println("**** CLASS RESOURCE PATH: $classResourcePath")
+/**
+ * Tells if this `URL` refers to a resource inside a JAR archive.
+ */
+private fun URL.isInJar() = toString().startsWith("jar")
+
+/**
+ * Loads a manifest "closest" to the given class.
+ *
+ * The location of the manifest file is obtained as one which has the most lengthy
+ * common prefix with the URL of the class.
+ *
+ * Please see [this Stack Overflow answer](https://stackoverflow.com/a/1273432/2395775)
+ * for details. Even though the answer covers the `"jar:"` case of a protocol, this function
+ * is meant to handle the URLs with the `"file:"` protocol (primarily used when we test).
+ * This is so because when we load a manifest from a JAR, we do it via
+ * [JarURLConnection.getManifest].
+ *
+ * @see KManifest.load
+ */
+private fun loadNonJar(cls: Class<*>): KManifest {
+    val classResourcePath = cls.toResourceUrl().toString()
     val manifestResource = Resource.file(KManifest.RESOURCE_FILE, cls.classLoader)
     val allManifests = manifestResource.locateAll()
-    println("**** ALL MANIFESTS: $allManifests")
     val urlToCommonPrefix = mutableMapOf<String, URL>()
     allManifests.forEach { url ->
         val commonPrefix = classResourcePath.commonPrefixWith(url.toString())
         urlToCommonPrefix[commonPrefix] = url
     }
     val longest = urlToCommonPrefix.keys.maxByOrNull { it.length }!!
-    println("**** LONGEST PREFIX: $longest")
     val nearestManifest = urlToCommonPrefix[longest]!!
-    println("**** NEAREST MANIFEST: $nearestManifest")
     val stream = nearestManifest.openStream()
     return KManifest.load(stream)
 }
