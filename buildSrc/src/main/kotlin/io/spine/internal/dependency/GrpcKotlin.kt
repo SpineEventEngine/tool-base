9e17911e9dedbb58689bd6b9431baf0957506c57
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-11-26 17:25:51 +0300

    Extract protoc plugin objects

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/GrpcKotlin.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/GrpcKotlin.kt
@@ -26,24 +26,17 @@
 
 package io.spine.internal.dependency
 
-// https://github.com/grpc/grpc-java
-@Suppress("unused")
-object Grpc {
-    @Suppress("MemberVisibilityCanBePrivate")
-    const val version        = "1.51.0"
-    const val api            = "io.grpc:grpc-api:${version}"
-    const val auth           = "io.grpc:grpc-auth:${version}"
-    const val core           = "io.grpc:grpc-core:${version}"
-    const val context        = "io.grpc:grpc-context:${version}"
-    const val stub           = "io.grpc:grpc-stub:${version}"
-    const val okHttp         = "io.grpc:grpc-okhttp:${version}"
-    const val protobuf       = "io.grpc:grpc-protobuf:${version}"
-    const val protobufLite   = "io.grpc:grpc-protobuf-lite:${version}"
-    const val netty          = "io.grpc:grpc-netty:${version}"
-    const val nettyShaded    = "io.grpc:grpc-netty-shaded:${version}"
+/**
+ * gRPC-Kotlin/JVM.
+ *
+ * @see <a href="https://github.com/grpc/grpc-kotlin">GitHub project</a>
+ */
+object GrpcKotlin {
+    const val version = "1.3.0"
+    const val stub = "io.grpc:grpc-kotlin-stub:$version"
 
     object ProtocPlugin {
-        const val id = "grpc"
-        const val artifact = "io.grpc:protoc-gen-grpc-java:${version}"
+        const val id = "grpckt"
+        const val artifact = "io.grpc:protoc-gen-grpc-kotlin:$version:jdk8@jar"
     }
 }
