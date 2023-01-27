258c408faee5ecd710200bb07ff7b295bd89b2cd
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-27 17:56:18 +0000

    Update `config`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Kotlin.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Kotlin.kt
@@ -41,6 +41,10 @@ object Kotlin {
 
     const val stdLib       = "${group}:kotlin-stdlib:${version}"
     const val stdLibCommon = "${group}:kotlin-stdlib-common:${version}"
+
+    @Deprecated("Please use `stdLib` instead.")
+    const val stdLibJdk7   = "${group}:kotlin-stdlib-jdk7:${version}"
+
     @Deprecated("Please use `stdLib` instead.")
     const val stdLibJdk8   = "${group}:kotlin-stdlib-jdk8:${version}"
 
