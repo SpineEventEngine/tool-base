8d4bf2b4908e454dfc42e216958669068f1d9d95
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-08-10 12:37:40 +0100

    Bump `logging` -> `2.0.0-SNAPSHOT.201`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -59,7 +59,7 @@ object Spine {
          *
          * @see <a href="https://github.com/SpineEventEngine/logging">spine-logging</a>
          */
-        const val logging = "2.0.0-SNAPSHOT.199"
+        const val logging = "2.0.0-SNAPSHOT.201"
 
         /**
          * The version of [Spine.testlib].
