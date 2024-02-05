257ef75550b5c5ac7cacadefc108850e10a19bf4
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-02-05 16:55:54 +0000

    Optimise imports

--- psi/src/main/kotlin/io/spine/tools/psi/PsiExts.kt
+++ psi/src/main/kotlin/io/spine/tools/psi/PsiExts.kt
@@ -27,8 +27,6 @@
 package io.spine.tools.psi
 
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.extensions.ExtensionPointName
-import com.intellij.openapi.extensions.ExtensionsArea
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.util.text.StringUtilRt
 import com.intellij.openapi.vfs.VirtualFile
