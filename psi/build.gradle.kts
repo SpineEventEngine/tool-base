241977347a3ce53537c8f571b7aceebf809f30f0
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-02-21 02:01:07 +0000

    Simplify dependency on `Spine.base`

--- psi/build.gradle.kts
+++ psi/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@
  */
 
 import io.spine.internal.dependency.IntelliJ
+import io.spine.internal.dependency.Spine
 
 repositories {
     intellijReleases
@@ -32,6 +33,7 @@ repositories {
 }
 
 dependencies {
+    api(Spine.base)
     IntelliJ.Platform.run {
         arrayOf(
             core,
