2426b983e55b9c50d6d66b1fa304afe623f10def
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-02-27 20:30:34 +0000

    Refactor and document dependencies and exclusions

--- psi-java/build.gradle.kts
+++ psi-java/build.gradle.kts
@@ -10,20 +10,36 @@ repositories {
 dependencies {
     implementation(Guava.lib)
     api(project(":psi"))
-    api(IntelliJ.Platform.core)
-    api(IntelliJ.Platform.util)
 
-    api(IntelliJ.Platform.codeStyle)
-    api(IntelliJ.Platform.codeStyleImpl)
-    api(IntelliJ.Platform.projectModel)
-    api(IntelliJ.Platform.projectModelImpl)
-    api(IntelliJ.Platform.lang)
+    with(IntelliJ.Platform) {
+        listOf(
+            core,
+            util,
+            codeStyle,
+            codeStyleImpl,
+            projectModel,
+            projectModelImpl,
+            lang,
+        ).forEach { api(it) }
+    }
 
+    api(IntelliJ.JavaPsi.api)
+    api(IntelliJ.JavaPsi.impl)
+
+    // The list of Maven groups we exclude when adding some IntelliJ artifact dependencies.
     val exclusions = listOf(
+        // These two are not called as transitive dependencies of IntelliJ API we call.
         "com.jetbrains.infra",
         "ai.grazie.spell",
+
+        // We add required IntelliJ Platform dependencies manually, as they are needed.
         "com.jetbrains.intellij.platform",
+
+        // Avoiding the clash with Gradle dependencies.
         "org.codehaus.groovy",
+
+        // Avoiding the clash with Gradle dependencies.
+        "org.slf4j"
     )
     fun ModuleDependency.excludeMany(excl: Iterable<String> = exclusions) {
         excl.forEach { exclude(it) }
@@ -37,16 +53,18 @@ dependencies {
     // To use `NonProjectFileWritingAccessProvider`, uncomment the following:
     api(IntelliJ.Platform.ideCoreImpl) { excludeMany() }
 
+    //
+    // Implementation dependencies on IntelliJ artifacts
+    //---------------------------------------------------
+
     // To access `com.intellij.psi.JspPsiUtil` as a transitive dependency
     // used by `com.intellij.psi.impl.source.codeStyle.ImportHelper`.
     implementation(IntelliJ.Jsp.jsp) { excludeMany() }
 
     implementation(IntelliJ.Xml.xmlPsiImpl) { excludeMany() }
+
     implementation(IntelliJ.Platform.analysisImpl) { excludeMany() }
     implementation(IntelliJ.Platform.indexingImpl) { excludeMany() }
-
-    api(IntelliJ.JavaPsi.api)
-    api(IntelliJ.JavaPsi.impl)
 
     implementation(IntelliJ.Java.impl) {
         excludeMany(listOf(
