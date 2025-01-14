71aadc38c61979a4fd725e78753e6deafbbf76e0
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-01-14 15:55:43 +0100

    Wrap `runnable` with `try-catch` before passing to PSI

--- psi-java/src/main/kotlin/io/spine/tools/psi/java/PsiCommands.kt
+++ psi-java/src/main/kotlin/io/spine/tools/psi/java/PsiCommands.kt
@@ -29,6 +29,7 @@ package io.spine.tools.psi.java
 import com.intellij.openapi.command.CommandProcessor
 import io.spine.tools.psi.java.Environment.commandProcessor
 import io.spine.tools.psi.java.Environment.project
+import java.lang.Thread.getDefaultUncaughtExceptionHandler
 
 /**
  * Executes the given [Runnable] as a PSI modification
@@ -36,5 +37,37 @@ import io.spine.tools.psi.java.Environment.project
  */
 @JvmName("execute")
 public fun execute(runnable: Runnable) {
-    commandProcessor.executeCommand(project, runnable, null, null)
+    val command = withRedirectedErrors(runnable)
+    commandProcessor.executeCommand(project, command, null, null)
+}
+
+/**
+ * Wraps the given [runnable] in a try-catch block, redirecting any [Throwable]
+ * to the default uncaught exception handler.
+ *
+ * The `CoreCommandProcessor` used by PSI for command handling swallows any
+ * [Throwable] from the passed [Runnable]. As a result, PSI users cannot know
+ * if an error has occurred. For example, ProtoData is expected to print
+ * the stacktrace and exist the application in case of error or exception,
+ * but this behavior is suppressed by PSI.
+ *
+ * This method addresses this issue by wrapping the given [runnable] in its own
+ * try-catch block, redirecting all errors and exceptions to the default handler.
+ * This ensures that PSI does not swallow these exceptions. However, as a consequence,
+ * the default exception handler must be explicitly set, as redirection only works
+ * when an explicit handler is in place.
+ *
+ * Note: to make this method work, the default exception handler must be set explicitly
+ * using [Thread.setDefaultUncaughtExceptionHandler]. Otherwise, a [NullPointerException]
+ * will be thrown and swallowed by PSI, making this method ineffective.
+ */
+@Suppress("TooGenericExceptionCaught")
+private fun withRedirectedErrors(runnable: Runnable) = Runnable {
+    try {
+        runnable.run()
+    } catch (e: Throwable) {
+        val currentThread = Thread.currentThread()
+        val globalHandler = getDefaultUncaughtExceptionHandler()
+        globalHandler.uncaughtException(currentThread, e)
+    }
 }
