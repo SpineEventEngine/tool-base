bf088ac30c36338552869f82f59d9798a3a01a65
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-03-20 18:30:39 +0000

    Improve dependencies

--- plugin-base/build.gradle.kts
+++ plugin-base/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import io.spine.dependency.lib.Kotlin
 import io.spine.dependency.lib.Protobuf
 import io.spine.dependency.local.Logging
 import io.spine.gradle.WriteVersions
@@ -35,7 +36,9 @@ plugins {
 dependencies {
     compileOnlyApi(gradleApi())
     compileOnlyApi(Protobuf.GradlePlugin.lib)
+    compileOnlyApi(Kotlin.GradlePlugin.api)
     api(project(":tool-base"))
+    compileOnly(gradleKotlinDsl())
     implementation(Logging.lib)
 
     testImplementation(project(":plugin-testlib"))
