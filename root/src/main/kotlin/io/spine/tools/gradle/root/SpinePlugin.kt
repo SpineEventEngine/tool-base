731317eb16bfde881d0bc2051df46b09c185a975
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-04-01 20:08:04 +0100

    Add `SpinePlugin` and `SpineSettingsPlugin`
    ... and corresponding extensions.

---
+++ root/src/main/kotlin/io/spine/tools/gradle/root/SpinePlugin.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.gradle.root
+
+import io.spine.tools.gradle.root.SpineProjectExtension.Companion.NAME
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.create
+
+/**
+ * Creates [SpineProjectExtension] in a project, if it is not already present.
+ */
+public class SpinePlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        project.run {
+            if (extensions.findByName(NAME) == null) {
+                extensions.create<SpineProjectExtension>(NAME)
+            }
+        }
+    }
+
+    public companion object {
+
+        /**
+         * The ID of the plugin.
+         */
+        public const val ID: String = "io.spine.root"
+    }
+}
---
+++ root/src/main/kotlin/io/spine/tools/gradle/root/SpineProjectExtension.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.gradle.root
+
+import org.gradle.api.plugins.ExtensionAware
+
+/**
+ * The root extension added by [SpinePlugin].
+ *
+ * This extension is used as a container for other extensions added by
+ * Gradle plugins of Spine libraries for configuration of compilation and other features.
+ *
+ * ### API Note
+ *
+ * Even though Gradle automatically makes a class created after the abstract extension
+ * class implement [ExtensionAware], we declare it explicitly to avoid the casts at
+ * the use sites in our code.
+ */
+public abstract class SpineProjectExtension : ExtensionAware {
+
+    public companion object {
+
+        /**
+         * The name of the project extension.
+         *
+         * This is the name that starts the `spine { }` configuration block in a project
+         * to which [SpinePlugin] is applied.
+         */
+        public const val NAME: String = "spine"
+    }
+}
