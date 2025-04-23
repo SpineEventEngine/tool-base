ceb1c2e14ee0cd0b44d12823519dd5bab18af2e3
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-04-23 13:08:53 +0100

    Introduce `Jupiter.apiArtifact` property

--- plugin-testlib/build.gradle.kts
+++ plugin-testlib/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,6 @@ dependencies {
     api(gradleApi())
     api(gradleTestKit())
     api(TestLib.lib)
-    api("${JUnit.Jupiter.api}:${JUnit.version}")
+    api(JUnit.Jupiter.apiArtifact)
     api(project(":plugin-base"))
 }
